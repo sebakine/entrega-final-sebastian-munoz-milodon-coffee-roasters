6eafc3a286fa96da95a8b3a5408918b18aff1e75
sebakine <sebakine.m@gmail.com>
2026-01-27 13:44:56 -0300

    mejoras de SEO para la entrega final coderhouse

--- CoffeeLink-web/CoffeeLink-backend/src/prisma/prisma.module.ts
+++
@@ -1,9 +0,0 @@
-import { Global, Module } from '@nestjs/common';
-import { PrismaService } from './prisma.service';
-
-@Global() // Makes PrismaService available everywhere without importing PrismaModule
-@Module({
-  providers: [PrismaService],
-  exports: [PrismaService],
-})
-export class PrismaModule {}
--- CoffeeLink-web/CoffeeLink-backend/src/users/users.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { UsersController } from './users.controller';
-import { PrismaService } from '../prisma/prisma.service';
-
-@Module({
-  controllers: [UsersController],
-  providers: [PrismaService], // Can add UsersService later for logic
-})
-export class UsersModule {}
